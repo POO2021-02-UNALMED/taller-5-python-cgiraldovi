5b868d92e1f20eaf54752244069756c983ffba3e
Alejandro <alemandoa@gmail.com>
2021-11-23 17:43:53 -0500

    Update testConstructores.py

--- testConstructores.py
+++ testConstructores.py
@@ -1,7 +1,11 @@
 from gestion.zona import Zona
-from gestion.zoologico import Zoologico
-from zooAnimales import Anfibio, Ave, Mamifero, Pez, Reptil, Animal
-
+from gestion.zoologico import Zoologico 
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.ave import Ave
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.pez import Pez
+from zooAnimales.reptil import Reptil
+from zooAnimales.animal import Animal
 
 def testZoologico():
     zoo = Zoologico("Central park", "Calle Principal")

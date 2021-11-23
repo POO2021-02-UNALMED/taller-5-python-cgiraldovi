948e05af433e19dc0d4fb56836f9dba823fba2ef
Alejandro <alemandoa@gmail.com>
2021-11-23 17:44:17 -0500

    Update testRetornos.py

--- testRetornos.py
+++ testRetornos.py
@@ -1,5 +1,11 @@
-from zooAnimales import Anfibio, Ave, Mamifero, Pez, Reptil, Animal
-
+from gestion.zona import Zona
+from gestion.zoologico import Zoologico 
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.ave import Ave
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.pez import Pez
+from zooAnimales.reptil import Reptil
+from zooAnimales.animal import Animal
 
 def testTotalTipo():
     Anfibio.crearRana("test", 11, "M")
@@ -20,7 +26,7 @@ def testTotalTipo():
     assert(ok)
 
 def testToString():
-    ave1 = Ave("paloma", 5, "ciudad", "F", "gris")
+    ave1 =Ave("paloma", 5, "ciudad", "F", "gris")
     ok = False
     comp = "Mi nombre es paloma, tengo una edad de 5, habito en ciudad y mi genero es F"
     if ave1.toString() ==  comp:
